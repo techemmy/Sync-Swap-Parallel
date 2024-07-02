03592523d09bd33d0ecac6c58dc0e976097df199
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-07-02 17:34:03 +0100

    project setup with typescript react tailwindcss shadcn/ui postcss autoprefixer vite react-icons react-router

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements([
+    <Route path='/' element={<App />} />
+  ])
+)
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
