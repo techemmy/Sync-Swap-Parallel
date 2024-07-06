27e68b3470792bde02d405ce6b22aad5ccd6429c
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-07-06 04:35:51 +0100

    style: change color format and add default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,10 +21,10 @@ export default {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
+        background: "rgb(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "rgb(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
@@ -75,4 +75,3 @@ export default {
   },
   plugins: [require("tailwindcss-animate")],
 };
-
