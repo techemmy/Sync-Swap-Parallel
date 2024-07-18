42d09ff76569f44984b6cbc14e0586c93742487b
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-07-18 20:27:27 +0100

    fix: make custom gradient show and add extra styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,22 +21,13 @@ export default {
         zKTextGradient: "linear-gradient(var(--zKTextGradient))",
         homePage: "linear-gradient(62deg,#c7c1ff,#ddc8ff)",
       },
-      keyframes: {
-        "portfolio-gradient": {
-          // not working yet
-          "0%": { "background-position": "0 50%" },
-          "50%": { "background-position": "100% 50%" },
-          "100%": { "background-position": "0 50%" },
-        },
-      },
-      animation: {
-        "portfolio-gradient": "portfolio-gradient 15s ease infinite",
-      },
       fontFamily: {
         Public: "Poppins, sans-serif",
       },
       boxShadow: {
+        DEFAULT: "var(--box-shadow)",
         light: "0px 0px 48px -50px",
+        hover: "var(--box-shadow-hover)",
       },
       colors: {
         navIcon: {
@@ -91,10 +82,16 @@ export default {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "portfolio-gradient": {
+          "0%": { "background-position": "0 50%" },
+          "50%": { "background-position": "100% 50%" },
+          "100%": { "background-position": "0 50%" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "portfolio-gradient": "portfolio-gradient 15s ease infinite",
       },
     },
   },
