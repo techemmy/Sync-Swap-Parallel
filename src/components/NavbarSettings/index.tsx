d933d4fa49923adbe1502cdd4f83c37940145e2e
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-07-29 23:01:26 +0100

    refactor: simplify logic for configuring settings view

--- src/components/NavbarSettings/index.tsx
+++ src/components/NavbarSettings/index.tsx
@@ -1,74 +1,33 @@
-import { useReducer } from "react";
+import { useState } from "react";
 import AdvancedView from "./SettingsMainOptions/AdvancedView";
 import ThemeView from "./SettingsMainOptions/ThemeView";
 import LanguageView from "./SettingsMainOptions/LanguageView";
 import DefaultSettingsView from "./DefaultSettingsView";
 import type { ISetSettingsView } from "@/types/Navbar";
 
-
-interface ISettingsOptionsState {
-    component: typeof DefaultSettingsView
-    | typeof ThemeView
-    | typeof LanguageView
-    | typeof AdvancedView;
-}
-
 enum SettingsOptionsEnum {
+    default = "default",
     theme = "theme",
     langugage = "language",
     advanced = "advanced"
 }
-
-interface ISettingsOptionsAction {
-    type?: SettingsOptionsEnum
-}
-
-function settingsOptionsReducer(state: ISettingsOptionsState, action: ISettingsOptionsAction) {
-    switch (action.type) {
-        case SettingsOptionsEnum.theme:
-            return {
-                ...state,
-                component: ThemeView
-            }
-
-        case SettingsOptionsEnum.langugage:
-            return {
-                ...state,
-                component: LanguageView
-            }
-
-        case SettingsOptionsEnum.advanced:
-            return {
-                ...state,
-                component: AdvancedView
-            }
-
-        default:
-            return {
-                ...state,
-                component: DefaultSettingsView
-            }
-    }
-
-}
-
 export default function NavbarSettings({ className = '' }: { className?: string }) {
-    const [state, dispatch] = useReducer(settingsOptionsReducer, {
-        component: DefaultSettingsView
-    });
-    const { component: CurrentView } = state;
+    const [settingsView, setSettingsView] = useState<SettingsOptionsEnum>(SettingsOptionsEnum.default);
 
 
-    const setSettingsView: ISetSettingsView = {
-        setDefault: () => dispatch({}),
-        setThemeAsView: () => dispatch({ type: SettingsOptionsEnum.theme }),
-        setLanguageAsView: () => dispatch({ type: SettingsOptionsEnum.langugage }),
-        setAdvancedAsView: () => dispatch({ type: SettingsOptionsEnum.advanced }),
+    const setSettingsViewDispatch: ISetSettingsView = {
+        setDefault: () => setSettingsView(SettingsOptionsEnum.default),
+        setThemeAsView: () => setSettingsView(SettingsOptionsEnum.theme),
+        setLanguageAsView: () => setSettingsView(SettingsOptionsEnum.langugage),
+        setAdvancedAsView: () => setSettingsView(SettingsOptionsEnum.advanced),
     }
 
     return (
         <section className={`min-w-[370px] rounded-3xl shadow-md bg-white p-4 pl-5 ${className}`}>
-            <CurrentView setSettingsView={setSettingsView} />
+            {settingsView === SettingsOptionsEnum.default && <DefaultSettingsView setSettingsView={setSettingsViewDispatch} />}
+            {settingsView === SettingsOptionsEnum.theme && <ThemeView setSettingsView={setSettingsViewDispatch} />}
+            {settingsView === SettingsOptionsEnum.langugage && <LanguageView setSettingsView={setSettingsViewDispatch} />}
+            {settingsView === SettingsOptionsEnum.advanced && <AdvancedView setSettingsView={setSettingsViewDispatch} />}
         </section>
     )
 }
