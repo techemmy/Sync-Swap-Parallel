7a22877b9088001213d90eee51a5f6991943ac0c
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-08-03 19:46:40 +0100

    fix: add info icon to tokens

--- src/components/SelectTokenDialog/TokenItem.tsx
+++ src/components/SelectTokenDialog/TokenItem.tsx
@@ -4,6 +4,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { MdOutlineInfo } from "react-icons/md";
 
 export interface TokenItemProps {
   logo: string;
@@ -13,12 +14,13 @@ export interface TokenItemProps {
 
 export default function TokenItem({ logo, symbol, name }: TokenItemProps) {
   return (
-    <Card className="shadow-none w-full border-0 px-3 hover:bg-accent cursor-pointer">
+    <Card className="group shadow-none w-full border-0 px-3 hover:bg-accent cursor-pointer">
       <CardHeader className="flex-row items-center gap-x-2 px-0 py-2">
         <img className="w-8 h-8 rounded-full" src={logo} alt={`${name} logo`} />
         <div>
-          <CardTitle className="text-md text-primary-foreground">
+          <CardTitle className="flex items-center gap-x-1 text-md text-primary-foreground">
             {symbol}
+            <MdOutlineInfo className="group-hover:block hidden" />
           </CardTitle>
           <CardDescription>{name}</CardDescription>
         </div>
