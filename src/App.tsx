d6659c4e591ca262a7985993a11ab4c74098738d
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-08-03 14:31:21 +0100

    style: format files

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,24 @@ import type { AppContextType, NetworkType } from "@/types/index";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
 export default function App() {
-  const [selectedNetwork, setSelectedNetwork] = useState<NetworkType>(Object.values(AVAILABLE_NETWORKS)[0]);
+  const [selectedNetwork, setSelectedNetwork] = useState<NetworkType>(
+    Object.values(AVAILABLE_NETWORKS)[0],
+  );
 
-  const outletContext = { selectedNetwork, setSelectedNetwork }
+  const outletContext = { selectedNetwork, setSelectedNetwork };
   return (
     <>
       <div className="bg-page">
         <TooltipProvider>
-          <NavBars selectedNetwork={selectedNetwork} setSelectedNetwork={setSelectedNetwork} />
+          <NavBars
+            selectedNetwork={selectedNetwork}
+            setSelectedNetwork={setSelectedNetwork}
+          />
           <Outlet context={outletContext satisfies AppContextType} />
         </TooltipProvider>
       </div>
     </>
-  )
+  );
 }
 
 export function useApp() {
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,44 +1,57 @@
 import Logo from "@/components/Logo";
 
 export default function Footer() {
-    return (
-        <footer className="md:px-32 pt-10 py-28 md:pb-32 grid grid-cols-2 md:grid-cols-5 gap-6 text-center md:text-left">
-            <Logo className="w-[48px] h-[48px] hidden md:block" />
+  return (
+    <footer className="md:px-32 pt-10 py-28 md:pb-32 grid grid-cols-2 md:grid-cols-5 gap-6 text-center md:text-left">
+      <Logo className="w-[48px] h-[48px] hidden md:block" />
 
-            <article className="grid gap-y-5">
-                <p className="text-card-foreground text-sm font-semibold">Community</p>
-                <ul className="text-primary-foreground grid gap-y-2">
-                    <a href="#"><li>Discord</li></a>
-                    <a href="#"><li>Guild</li></a>
-                </ul>
-            </article>
+      <article className="grid gap-y-5">
+        <p className="text-card-foreground text-sm font-semibold">Community</p>
+        <ul className="text-primary-foreground grid gap-y-2">
+          <a href="#">
+            <li>Discord</li>
+          </a>
+          <a href="#">
+            <li>Guild</li>
+          </a>
+        </ul>
+      </article>
 
-            <article className="grid gap-y-5">
-                <p className="text-card-foreground text-sm font-semibold">News</p>
-                <ul className="text-primary-foreground grid gap-y-2">
-                    <a href="#"><li>Twitter</li></a>
-                    <a href="#"><li>Mirror</li></a>
-                </ul>
-            </article>
+      <article className="grid gap-y-5">
+        <p className="text-card-foreground text-sm font-semibold">News</p>
+        <ul className="text-primary-foreground grid gap-y-2">
+          <a href="#">
+            <li>Twitter</li>
+          </a>
+          <a href="#">
+            <li>Mirror</li>
+          </a>
+        </ul>
+      </article>
 
-            <article className="grid gap-y-5">
-                <p className="text-card-foreground text-sm font-semibold">Resources</p>
-                <ul className="text-primary-foreground grid gap-y-2">
-                    <a href="#"><li>Docs</li></a>
-                    <a href="#"><li>Github</li></a>
-                </ul>
-            </article>
+      <article className="grid gap-y-5">
+        <p className="text-card-foreground text-sm font-semibold">Resources</p>
+        <ul className="text-primary-foreground grid gap-y-2">
+          <a href="#">
+            <li>Docs</li>
+          </a>
+          <a href="#">
+            <li>Github</li>
+          </a>
+        </ul>
+      </article>
 
-            <article className="grid gap-y-5">
-                <p className="text-card-foreground text-sm font-semibold">Links</p>
-                <ul className="text-primary-foreground grid gap-y-2">
-                    <a href="#"><li>Portal</li></a>
-                    <a href="#"><li>Block Explorer</li></a>
-                </ul>
-            </article>
-
-
-        </footer>
-    )
+      <article className="grid gap-y-5">
+        <p className="text-card-foreground text-sm font-semibold">Links</p>
+        <ul className="text-primary-foreground grid gap-y-2">
+          <a href="#">
+            <li>Portal</li>
+          </a>
+          <a href="#">
+            <li>Block Explorer</li>
+          </a>
+        </ul>
+      </article>
+    </footer>
+  );
 }
-
