78d1dc1cd6bd2dc90bb1621d80e6bd7c8658d505
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-08-03 19:42:46 +0100

    refactor: improve token icons accessibility

--- src/components/SelectTokenDialog/TokenHighlight.tsx
+++ src/components/SelectTokenDialog/TokenHighlight.tsx
@@ -6,7 +6,11 @@ interface TokenHighlightProps {
 export default function TokenHighlight({ name, logoSrc }: TokenHighlightProps) {
   return (
     <li className="flex items-center gap-x-1 text-sm font-medium py-1 px-2 hover:bg-accent cursor-pointer rounded-xl">
-      <img className="w-5 h-5 rounded-full" src={logoSrc} />
+      <img
+        className="w-5 h-5 rounded-full"
+        src={logoSrc}
+        alt={`${name} logo`}
+      />
       {name}
     </li>
   );
--- src/components/SelectTokenDialog/TokenItem.tsx
+++ src/components/SelectTokenDialog/TokenItem.tsx
@@ -15,7 +15,7 @@ export default function TokenItem({ logo, symbol, name }: TokenItemProps) {
   return (
     <Card className="shadow-none w-full border-0 px-3 hover:bg-accent cursor-pointer">
       <CardHeader className="flex-row items-center gap-x-2 px-0 py-2">
-        <img className="w-8 h-8 rounded-full" src={logo} />
+        <img className="w-8 h-8 rounded-full" src={logo} alt={`${name} logo`} />
         <div>
           <CardTitle className="text-md text-primary-foreground">
             {symbol}
