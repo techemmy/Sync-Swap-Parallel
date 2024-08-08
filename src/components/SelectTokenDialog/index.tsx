c31474adb5172a2551654af3c4f17fdfd3bb80ca
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-08-08 05:27:27 +0100

    fix: make radius more like original

--- src/components/SelectTokenDialog/index.tsx
+++ src/components/SelectTokenDialog/index.tsx
@@ -36,7 +36,7 @@ export default function SelectTokenDialog() {
   }, [searchTokenInput]);
 
   return (
-    <DialogContent className="flex flex-col bg-card p-0 sm:max-w-md border-0 h-[90vh] gap-2">
+    <DialogContent className="flex flex-col bg-card p-0 sm:max-w-md border-0 h-[90vh] gap-2 sm:rounded-3xl">
       <DialogHeader className="bg-primary/[0.16] px-5 pt-5 border-0 text-primary-foreground">
         <DialogTitle className="font-medium flex items-center">
           Select a token
