a62419f02bbf22236381914a2d7d508cc723e40e
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-09-19 19:40:55 +0100

    refactor: handle random data better

--- src/pages/pools/PoolTableDisplay.tsx
+++ src/pages/pools/PoolTableDisplay.tsx
@@ -7,24 +7,33 @@ import {
 } from "@/components/ui/select";
 import Tokens from "@/assets/tokens.json";
 import { Card, CardContent } from "@/components/ui/card";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BsTriangleHalf } from "react-icons/bs";
 import { FiArrowRight } from "react-icons/fi";
 import { MdElectricBolt, MdOutlineWaterDrop } from "react-icons/md";
 import { TbAlignBoxRightBottom } from "react-icons/tb";
 import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
 
-interface IToken {
-  symbol: string;
-  logo: string;
+interface IPoolType {
+  icon: React.ReactNode;
+  title: string;
 }
 
-interface PoolCardProp {
-  firstToken: IToken;
-  secondToken: IToken;
+interface IPoolData {
+  firstToken: {
+    symbol: string;
+    logo: string;
+  };
+  secondToken: {
+    symbol: string;
+    logo: string;
+  };
+  poolType: IPoolType;
+  liquidity: string;
+  APR: string;
 }
 
-const poolTypes = [
+const poolTypes: IPoolType[] = [
   {
     icon: <MdOutlineWaterDrop fontSize={23} />,
     title: "Aqua",
@@ -39,12 +48,15 @@ const poolTypes = [
   },
 ];
 
-function PoolCard({ firstToken, secondToken }: PoolCardProp) {
-  const type = Math.floor(Math.random() * 10) % 3;
-  const poolType = poolTypes[type];
+function PoolCard({ pool }: { pool: IPoolData }) {
+  const { firstToken, secondToken, poolType, liquidity, APR } = pool;
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <Card className="animate-fade-in border-transparent cursor-pointer bg-white/70 hover:bg-white">
-      <CardContent className="px-4 py-2 grid grid grid-cols-[1.65fr_1.15fr_0.9fr_0.7fr_1.1fr] items-center  text-sm font-medium text-primary-foreground">
+      <CardContent
+        onClick={() => setIsOpen(!isOpen)}
+        className="px-4 py-2 grid grid grid-cols-[1.65fr_1.15fr_0.9fr_0.7fr_1.1fr] items-center  text-sm font-medium text-primary-foreground"
+      >
         <section className="flex gap-x-1">
           <div className="flex gap-x-2 items-center border border-primary/30 rounded-3xl px-2 py-1">
             <img
@@ -54,7 +66,6 @@ function PoolCard({ firstToken, secondToken }: PoolCardProp) {
             />
             {firstToken.symbol}
           </div>
-
           <div className="flex gap-x-2 items-center border border-primary/30 rounded-3xl px-2 py-1">
             <img
               className="w-7 h-7 rounded-full"
@@ -68,35 +79,44 @@ function PoolCard({ firstToken, secondToken }: PoolCardProp) {
           {poolType.icon}
           {poolType.title}
         </p>
-        <p>${(Math.random() * 10000000).toLocaleString()}</p>
+        <p>${liquidity}</p>
         <p className="ml-auto flex gap-x-2 items-center">
-          <MdElectricBolt /> <span>{(Math.random() * 10).toFixed(2)} %</span>
+          <MdElectricBolt /> <span>{APR} %</span>
         </p>
         <div className="ml-auto w-max p-2 border border-primary text-primary rounded-lg transition-colors active:bg-accent">
           <FiArrowRight fontSize={20} />
         </div>
       </CardContent>
+      {isOpen && <div>open</div>}
     </Card>
   );
 }
 
-const TOKENS = Object.values(Tokens.data).map(({ symbol, logo }: IToken) => ({
-  symbol,
-  logo,
-}));
+const TOKENS = Object.values(Tokens.data).map(
+  ({ symbol, logo }: { symbol: string; logo: string }) => ({
+    symbol,
+    logo,
+  }),
+);
 
-let pools = [] as Array<IToken[]>;
+let pools = [] as Array<IPoolData>;
 for (let i = 0; i < TOKENS.length; i += 2) {
   const from = TOKENS[i++];
   const to = TOKENS[i++];
-  pools.push([from, to]);
+
+  const type = Math.floor(Math.random() * 10) % 3;
+  const poolType = poolTypes[type];
+
+  const liquidity = (Math.random() * 10000000).toLocaleString();
+  const APR = (Math.random() * 10).toFixed(2);
+  pools.push({ firstToken: from, secondToken: to, poolType, liquidity, APR });
 }
 
 export default function PoolTableDisplay() {
   const [page, setPage] = useState(1);
   const [pageSize, setPageSize] = useState(10);
   const pageStart = (page - 1) * pageSize;
-  const [poolsData, setPoolsData] = useState<IToken[][]>(
+  const [poolsData, setPoolsData] = useState<IPoolData[]>(
     pools.slice(pageStart, pageStart + pageSize),
   );
 
@@ -128,16 +148,9 @@ export default function PoolTableDisplay() {
   return (
     <section>
       <div className="grid gap-y-3">
-        {poolsData.map((pool, index) => {
-          const [firstToken, secondToken] = pool;
-          return (
-            <PoolCard
-              key={firstToken.symbol + secondToken.symbol + index}
-              firstToken={firstToken}
-              secondToken={secondToken}
-            />
-          );
-        })}
+        {poolsData.map((pool, index) => (
+          <PoolCard key={pool.firstToken.symbol + index} pool={pool} />
+        ))}
       </div>
 
       <div className="mt-5 flex justify-between">
