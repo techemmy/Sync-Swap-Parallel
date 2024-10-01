8a5984031217e9790d71496c1d9c748caabb4199
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-10-01 15:24:47 +0100

    refactor: use cn lib to combine class names

--- src/components/ConnectWalletDialogButton.tsx
+++ src/components/ConnectWalletDialogButton.tsx
@@ -2,6 +2,7 @@ import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { IoWalletOutline } from "react-icons/io5";
 import ConnectWalletDialog from "@/components/ConnectWalletDialog";
 import { Button, ButtonProps } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 interface Props extends ButtonProps {
   className?: string;
@@ -16,7 +17,10 @@ export default function ConnectWalletDialogButton({
       <DialogTrigger asChild>
         <Button
           {...props}
-          className={`rounded-xl flex gap-x-2 transition-all hover:shadow active:scale-90 mt-2 border-primary/[0.5] ${className}`}
+          className={cn(
+            "rounded-xl flex gap-x-2 transition-all hover:shadow active:scale-90 mt-2 border-primary/[0.5]",
+            className,
+          )}
         >
           <IoWalletOutline size={20} /> Connect
         </Button>
