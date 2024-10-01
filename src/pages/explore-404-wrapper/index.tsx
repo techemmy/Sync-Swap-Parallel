30ad8e40742510b6e52fa34931265150cae2509a
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-10-01 19:19:28 +0100

    refactor: use FAQs component

--- src/pages/explore-404-wrapper/index.tsx
+++ src/pages/explore-404-wrapper/index.tsx
@@ -1,15 +1,57 @@
 import Footer from "@/components/Footer";
 import WelcomeCard from "./WelcomeCard";
 import FeaturedCollection from "./FeaturedCollection";
-import FAQ from "./FAQ";
+import FAQs from "@/components/FAQs";
 
 export default function Explore404Wrapper() {
+  const faqs = [
+    {
+      question: "What is 404 Wrapper?",
+      answer:
+        "Inspired by ERC404, the SyncSwap 404 Wrapper introduces a new technology to wrap existing ERC721 NFTs into fungible 404 tokens. These transformed tokens can seamlessly integrate into DeFi apps, functioning just like standard ERC20 tokens.",
+    },
+    {
+      question: "Why 404 Wrapper?",
+      answer:
+        "By wrapping your NFTs into the 404 fungible tokens, you unlock the ability to trade and pool them on SyncSwap and much more, significantly enhancing their liquidity and expanding their utility across various ways. Enjoy reduced fees, immediate trading capabilities, and opportunities for earning through liquidity pools.",
+    },
+    {
+      question: "What is LIBERTAS OMNIBUS?",
+      answer:
+        "LIBERTAS OMNIBUS is an experimental NFT project by the zkSync team to explore new NFT standards, network superpowers, new protocol features, prototype collections with surprise creators, and just vibe out.",
+    },
+    {
+      question: "Are There Any Fees?",
+      answer:
+        "The 404 Wrapper operates on a fully permissionless and open-source basis, there are no fees for wrapping and unwrapping NFTs.",
+    },
+    {
+      question: "Can I Trade the Wrapped 404 Tokens?",
+      answer:
+        "Yes. You can trade the wrapped tokens as you would with regular ERC20 tokens on SyncSwap. You can even provide liquidity for it to earn from fees and rewards.",
+    },
+    {
+      question: "How Do I Wrap My NFT into the 404 Token?",
+      answer:
+        "Getting started is simple. Just click the Get Started button and follow the provided instructions to approve and wrap your NFT. It's a hassle-free process!",
+    },
+    {
+      question: "How Can I Unwrap My 404 Token into an NFT?",
+      answer:
+        "To unwrap a 404 token back into an NFT, click the Get Started button and follow the instructions. Keep in mind that since NFTs cannot be fractioned, you'll need at least one complete token to unwrap.",
+    },
+    {
+      question: "Can I Receive the Same NFT Back Upon Unwrapping?",
+      answer:
+        "The wrapper pools NFTs together, so each unwrapping will yield a randomly selected NFT from the pool. As a result, you may receive a different NFT, making it particularly suitable for NFTs that are unrevealed or with fewer rare attributes.",
+    },
+  ];
   return (
     <>
       <main className="relative overflow-auto pt-28 pb-20 min-h-screen max-w-screen-lg mx-auto space-y-14 p-4">
         <WelcomeCard />
         <FeaturedCollection />
-        <FAQ />
+        <FAQs faqs={faqs} />
       </main>
 
       <Footer />
--- src/pages/explore-usdc-conversion/index.tsx
+++ src/pages/explore-usdc-conversion/index.tsx
@@ -1,15 +1,66 @@
 import Footer from "@/components/Footer";
 import LearnMoreCard from "./LearnMoreCard";
 import ConvertUSDC from "./ConvertUSDC";
-import FAQ from "./FAQ";
+import FAQs from "@/components/FAQs";
 
 export default function ExploreUSDCConversion() {
+  const faqs = [
+    {
+      question: "What is USDC conversion?",
+      answer:
+        "USDC conversion helps you convert your USDC.e to the new native USDC token easily in a 1:1 fixed rate. SyncSwap is leading the USDC conversion process by providing an easy to use converter UI.",
+    },
+    {
+      question: "What is native USDC?",
+      answer:
+        "Circle - the issuer of USDC, is launching the native USDC on zkSync. Until now, all USDC on zkSync has been bridged through the zkSync bridge from Ethereum and backed by USDC on Ethereum mainnet. Native USDC is different. It's backed by Circle with additional benefits.",
+    },
+    {
+      question: "What is USDC.e?",
+      answer:
+        "USDC.e is the USDC token bridged with the zkSync bridge from the Ethereum mainnet. It was labeled as USDC simply in the past year, however it is now renamed as USDC.e to support the native USDC launch.",
+    },
+    {
+      question: "How can I use my native USDC?",
+      answer:
+        "You can use your USDC as usual, swapping and providing liquidity to earn and unlock more opportunities. There is no significant change. Note some other dApps may only support USDC.e in early days.",
+    },
+    {
+      question: "When will I receive native USDC after conversion?",
+      answer:
+        "You will receive the new USDC token in your wallet immediately once the transaction is confirmed.",
+    },
+    {
+      question: "Are there any fees to convert my USDC.e?",
+      answer: "There is no fee in the USDC conversion process.",
+    },
+    {
+      question: "Can I convert my native USDC back to USDC.e?",
+      answer:
+        "The converter is one-way. You can only convert USDC.e to native USDC with the converter. You can access the liquidity on SyncSwap to swap them back to USDC.e.",
+    },
+    {
+      question: "Who is the initiator of the USDC conversion?",
+      answer:
+        "The native USDC launch and the USDC.e conversion is initiated officially by zkSync and Circle. SyncSwap is one of the launch partners of the native USDC.",
+    },
+    {
+      question: "Is the converter contract safe?",
+      answer:
+        "The USDC converter contract is deployed and funded by the zkSync team. You are interacting with the zkSync official contract directly.",
+    },
+    {
+      question: "Why am I seeing Insufficient USDC balance in converter?",
+      answer:
+        "The converter is funded by the zkSync team and it may run out of native USDC tokens, please come again later. You could still swap with liquidity pools on SyncSwap.",
+    },
+  ];
   return (
     <>
       <main className="relative overflow-auto pt-28 pb-20 min-h-screen max-w-screen-xl mx-auto space-y-14 p-4">
         <LearnMoreCard />
         <ConvertUSDC />
-        <FAQ />
+        <FAQs faqs={faqs} />
       </main>
 
       <Footer />
