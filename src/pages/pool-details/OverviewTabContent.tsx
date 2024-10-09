ff13ccdac12b27c2663931e5bec453a6c87b41d0
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-10-09 10:08:03 +0100

    style: change file folder

--- src/pages/pool-details/OverviewTabContent.tsx
+++ src/pages/pool-details/OverviewTabContent.tsx
@@ -1,9 +1,9 @@
 import PoolTypeDetails from "@/components/pool-type-details";
 import { TabsContent } from "@/components/ui/tabs";
 
-export default function Overview() {
+export default function OverviewTabContent() {
   return (
-    <TabsContent value="overview" className="w-full">
+    <TabsContent value="overview" className="w-full mt-0">
       <PoolTypeDetails poolType="Classic" className="pt-0" />
     </TabsContent>
   );
